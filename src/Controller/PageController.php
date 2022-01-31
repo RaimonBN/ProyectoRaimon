12df6e793961d0c5046ffbdac803b47adaa5e1ec
Raimon <raimonbn2001@gmail.com>
2022-01-31 09:44:00 +0100

    first commit

---
+++ src/Controller/PageController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    /**
+     * @Route("/page", name="page")
+     */
+    public function index(): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+      /**
+     * @Route("/", name="inicio")
+     */
+    public function inicio(): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+}
